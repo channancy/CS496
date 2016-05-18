7169e35d9f5f697dfadb950ddb1af22f17abf51a
Nancy Chan <nancychannc@gmail.com>
2016-05-17 22:31:36 -0700

    Added delete request

--- app/src/main/java/example/com/dreamshare/DreamAdapter.java
+++ app/src/main/java/example/com/dreamshare/DreamAdapter.java
@@ -1,6 +1,8 @@
 package example.com.dreamshare;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,6 +10,11 @@ import android.widget.ArrayAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 import java.util.ArrayList;
 
 /**
@@ -66,10 +73,81 @@ public class DreamAdapter extends ArrayAdapter<Dream> implements View.OnClickLis
                 Toast.makeText(c, "Clicked Edit", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.tvDelete:
+
+                DeleteTask deleteTask = new DeleteTask(c);
+                deleteTask.execute();
+
                 Toast.makeText(c, "Clicked Delete", Toast.LENGTH_SHORT).show();
+
                 break;
             default:
                 break;
+        }
+    }
+
+    // AsyncTask (do not perform networking operation on the main thread)
+    private class DeleteTask extends AsyncTask<Void, Void, Void> {
+
+        private Context mContext;
+        private ProgressDialog mProgressDialog;
+
+        public DeleteTask(Context context) {
+            mContext = context;
+        }
+
+        // Show loading message
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            mProgressDialog = new ProgressDialog(mContext);
+            mProgressDialog.setMessage("Deleting dream...");
+            mProgressDialog.setIndeterminate(false);
+            mProgressDialog.setCancelable(true);
+            mProgressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            URL url = null;
+            try {
+                url = new URL("http://dreamshare2-1314.appspot.com/dream/5644406560391168");
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            HttpURLConnection httpCon = null;
+            try {
+                httpCon = (HttpURLConnection) url.openConnection();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            httpCon.setDoOutput(true);
+            httpCon.setRequestProperty(
+                    "Content-Type", "application/x-www-form-urlencoded" );
+            try {
+                httpCon.setRequestMethod("DELETE");
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            }
+            try {
+                httpCon.connect();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                httpCon.getInputStream();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void param) {
+            // Close loading message
+            mProgressDialog.dismiss();
+
         }
     }
 }
